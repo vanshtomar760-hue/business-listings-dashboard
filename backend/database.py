19767048ad421c0e3c34796e38077582774bffb0
Vansh Tomar <vanshtomar760@gmail.com>
2026-05-22 13:08:14 +0530

    Fixed backend dependency issue

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "mysql+pymysql://root:Happy87911%40%40@localhost/business_dashboard"
+DATABASE_URL = "mysql+pymysql://root:@localhost/business_dashboard"
 
 engine = create_engine(DATABASE_URL)
 
